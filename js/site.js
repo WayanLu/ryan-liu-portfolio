a7a7c71703afd9819e32e6e1b90d1a0013e46597
WayanLu <ryliu3@gmail.com>
2024-08-26 13:14:35 -0700

    main skeleton done

---
+++ js/site.js
@@ -0,0 +1,18 @@
+// js/site.js
+
+window.onload = function() {
+    // Load header and footer
+    fetch('../html/header.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('header').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading header:', error));
+
+    fetch('../html/footer.html')
+        .then(response => response.text())
+        .then(data => {
+            document.getElementById('footer').innerHTML = data;
+        })
+        .catch(error => console.error('Error loading footer:', error));
+};
